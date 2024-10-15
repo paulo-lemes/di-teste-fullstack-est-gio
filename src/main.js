dc09c8ec1b2cf8741469584a3cc6fcd24233c431
Paulo Lemes <paulo-lemes@live.com>
2024-10-15 19:22:47 -0300

    add try-catch, error ctx reply

--- src/main.js
+++ src/main.js
@@ -21,39 +21,43 @@ async function getLowestPrice(ctx) {
   // const { message } = ctx.update;
 
   await ctx.reply("Buscando dados...");
-  const response = await axios.get(`https://${url.source}`);
 
-  const $ = cheerio.load(response.data);
-  const plans = [];
+  try {
+    const response = await axios.get(`https://${url.source}`);
 
-  $(".generic_head_price").each((index, element) => {
-    const name = $(element).find(".head").text().trim();
-    const price = parseFloat($(element).find(".currency").text().trim());
-    if (price > 0) {
-      plans.push({ name, price });
+    const $ = cheerio.load(response.data);
+    const plans = [];
+
+    $(".generic_head_price").each((index, element) => {
+      const name = $(element).find(".head").text().trim();
+      const price = parseFloat($(element).find(".currency").text().trim());
+      if (price > 0) {
+        plans.push({ name, price });
+      }
+    });
+
+    if (!plans.length) {
+      await ctx.reply("Nenhum plano encontrado");
+      return;
     }
-  });
 
-  console.log("Plans: ", plans);
+    const lowestPaidPlan = plans.reduce((lowest, current) =>
+      current.price < lowest.price ? current : lowest
+    );
 
-  if (!plans.length) {
-    await ctx.reply("Nenhum plano encontrado.");
-    return;
+    const lowestPlanName = lowestPaidPlan.name;
+    const lowestPlanPrice = lowestPaidPlan.price.toLocaleString("pt-BR", {
+      style: "currency",
+      currency: "BRL",
+    });
+
+    await ctx.reply(
+      `O plano pago mais barato é o "${lowestPlanName}" e custa "${lowestPlanPrice}"`
+    );
+  } catch (err) {
+    console.error("Erro", err);
+    await ctx.reply("Erro ao buscar dados");
   }
-
-  const lowestPaidPlan = plans.reduce((lowest, current) =>
-    current.price < lowest.price ? current : lowest
-  );
-
-  const lowestPlanName = lowestPaidPlan.name;
-  const lowestPlanPrice = lowestPaidPlan.price.toLocaleString("pt-BR", {
-    style: "currency",
-    currency: "BRL",
-  });
-
-  await ctx.reply(
-    `O plano pago mais barato é o "${lowestPlanName}" e custa "${lowestPlanPrice}"`
-  );
 }
 
 main();
